f0a8138d52c3498d4a7c86e0c706bf77f97ad6bd
ReplitBot <replit@replit.com>
2025-09-19 11:26:06 +0000

    Update bot with Railway backup support

--- utils/level.js
+++ utils/level.js
@@ -6,7 +6,7 @@ const { uploadToDropbox, downloadFromDropbox, ensureDropboxInit } = require('./s
 const { ChannelType } = require('discord.js');
 
 const LEVEL_SETTINGS_PATH = path.join(__dirname, '../levels.json');
-const DROPBOX_LEVEL_DATA_PATH = '/app-/userLevels.json';
+const DROPBOX_LEVEL_DATA_PATH = '/app-data/userLevels.json';
 
 let userLevels = {};
 let levelSettings = {};
